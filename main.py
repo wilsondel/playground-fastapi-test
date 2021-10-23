0f0c5115d24410517cc666c995d30eb393cdff4b
Wilson <wilson.delgadoher@gmail.com>
2021-10-23 11:19:54 -0500

    validations_query_parameters

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 #Fast API
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 
 app = FastAPI()
@@ -29,3 +29,14 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)): # ... -> significa que es obligatorio
     return person
+
+# Validaciones: Query parameters
+# /person/detail?name=Miguel%age=25
+# Query(...,) -> no es lo ideal, solo se hace para saber que si llega
+# a suceder que un query parameter sea obligatorio, y se cambia Optional.
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: Optional[str] = Query(...)
+):
+    return {name:age}
